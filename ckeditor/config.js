8293aa5229fa013ef87122e9469f1e70a0befaea
hbl917070 <hbl917070@hotmail.com.tw>
2017-05-18 23:33:47 +0800

    8

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -4,32 +4,30 @@
  */
 
 CKEDITOR.editorConfig = function( config ) {
-	config.toolbarGroups = [
-		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
-		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
-		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
-		{ name: 'forms', groups: [ 'forms' ] },
+    config.toolbarGroups = [
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'forms', groups: ['forms'] },
 		'/',
-		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
-		{ name: 'links', groups: [ 'links' ] },
-		{ name: 'insert', groups: [ 'insert' ] },
-		{ name: 'styles', groups: [ 'styles' ] },
-		{ name: 'colors', groups: [ 'colors' ] },
-		{ name: 'tools', groups: [ 'tools' ] },
-		{ name: 'others', groups: [ 'others' ] },
-		{ name: 'about', groups: [ 'about' ] }
-	];
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'tools', groups: ['tools'] },
+		{ name: 'others', groups: ['others'] },
+		{ name: 'about', groups: ['about'] }
+    ];
 
-	config.removeButtons = 'Source,NewPage,Preview,Print,Save,Cut,Undo,Paste,PasteText,PasteFromWord,Copy,Redo,Replace,Find,SelectAll,Scayt,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Superscript,Subscript,Image,Flash,Smiley,PageBreak,SpecialChar,Iframe,Styles,Format,ShowBlocks,About,Link,Unlink,Anchor,Language,BidiRtl,BidiLtr,CreateDiv,Blockquote,Templates,CopyFormatting,Maximize';
-	
-	config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,Cut,Undo,Redo,Paste,PasteText,Copy,PasteFromWord,Replace,SelectAll,Scayt,Form,Checkbox,Radio,Textarea,Select,Button,ImageButton,HiddenField,TextField,Blockquote,CreateDiv,Language,BidiLtr,Link,Unlink,Anchor,Flash,SpecialChar,Iframe,Maximize,About,Find,Superscript,Subscript,BidiRtl,Smiley,Styles,Format,PageBreak';
-	
+    config.removeButtons = 'Source,Save,NewPage,Preview,Print,Templates,Cut,Undo,Redo,Paste,PasteText,Copy,PasteFromWord,Replace,SelectAll,Scayt,Form,Checkbox,Radio,Textarea,Select,Button,ImageButton,HiddenField,TextField,Blockquote,CreateDiv,Language,BidiLtr,Link,Unlink,Anchor,Flash,SpecialChar,Iframe,Maximize,About,Find,Superscript,Subscript,BidiRtl,Smiley,Styles,Format,PageBreak,Image,CopyFormatting';
+
 	//字型size
-	config.fontSize_sizes = '12/12;16/16;20/20;24/24;36/36;48/48;60/60;72/72;90/90;';
+	config.fontSize_sizes = '12/12px;16/16px;20/20px;24/24px;36/36px;48/48px;60/60px;72/72px;90/90px;';
 	
 	//字體
-	config.font_names = 'Konosuba;微軟正黑體;新細明體;標楷體;Arial;Arial Black;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana';
+	config.font_names = 'KonosubaMK3;微軟正黑體;新細明體;標楷體;Arial;Arial Black;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana';
 	
 	//還原次數
 	config.undoStackSize = 50;
